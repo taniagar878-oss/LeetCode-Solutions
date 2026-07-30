8044d39cfb1ec3868a88ac878641d7fc04b10513
Tanishtha Agarwal <taniagar878@gmail.com>
2026-07-30 15:10:57 +0530

    Time: 0 ms (100%), Space: 12.9 MB (17.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,23 +4,21 @@ public:
     {
         if(index==nums.size())
         {
+            ans.push_back(sub);
             return;
         }
-        if(index<nums.size())
-        {
-            Subset(index+1,nums,sub,ans);
-            sub.push_back(nums[index]);
-            ans.push_back(sub);
-            Subset(index+1,nums,sub,ans);
-            sub.pop_back();
-        }
+       
+        
+        Subset(index+1,nums,sub,ans);
+        sub.push_back(nums[index]);
+        Subset(index+1,nums,sub,ans);
+        sub.pop_back();
     }
     vector<vector<int>> subsets(vector<int>& nums) 
     {
         vector<vector<int>>ans;
         vector<int>sub;
         Subset(0,nums,sub,ans);
-        ans.push_back({});
         return ans;
     }
 };
