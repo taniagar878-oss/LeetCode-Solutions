db59abdeba2bcdfa7593c464c7d7ac0eb930c1cc
Tanishtha Agarwal <taniagar878@gmail.com>
2026-08-21 22:11:59 +0530

    Time: 0 ms (100%), Space: 23.2 MB (20.78%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,26 +11,20 @@
  */
 class Solution {
 public:
+    int height(TreeNode* root)
+    {
+        if(root==NULL) return 0;
 
-    int height(TreeNode* root) {
-        if (root == NULL)
-            return 0;
+        int left=height(root->left);
+        int right=height(root->right);
 
-        int left = height(root->left);
-        if (left == -1)
-            return -1;
+        if(left==-1 || right==-1) return -1;
+        if(abs(left-right)>1) return -1;
 
-        int right = height(root->right);
-        if (right == -1)
-            return -1;
-
-        if (abs(left - right) > 1)
-            return -1;
-
-        return 1 + max(left, right);
+        return 1+(max(left,right));
     }
-
-    bool isBalanced(TreeNode* root) {
-        return height(root) != -1;
+    bool isBalanced(TreeNode* root) 
+    {
+        return height(root)!=-1;
     }
 };
