1eb7f85fd83a23aa99e3c4abfa5c9f44f59ae4ae
Tanishtha Agarwal <taniagar878@gmail.com>
2026-08-26 14:22:03 +0530

    Time: 4 ms (0.54%), Space: 10.1 MB (9.41%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -11,19 +11,13 @@ public:
             dp[i][0]=1;
             dp[i][index++]=1;
         }
-        for(int i=2; i<n; i++)
-        {
-            for(int j=1; j<i; j++)
-            {
-                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-            }
-        }
         ans.push_back({1});
         for(int i=1; i<n; i++)
         {
             vector<int>sub={1};
             for(int j=1; j<i; j++)
             {
+                dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
                 sub.push_back(dp[i][j]);
             }
             sub.push_back(1);
